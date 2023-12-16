400d6edbb97768f9e752ee128a57c083721165b6
Kalki <kalkithewarrior2706@proton.me>
2023-12-16 14:48:48 +0530

    Update panic errors

--- Concepts/panic_errors/src/main.rs
+++ Concepts/panic_errors/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     // Calling panic! macro to see error
     // panic!("crash and burn!");
 
-    // Using panic! backtrace with calling value in vector beyond it's indexes.
+    // Using panic! backtrace with calling value in vector beyond it's indexes. Read more about panic errors in Chapter 9.
     let v = vec![1, 2, 3];
     v[99];
 }
