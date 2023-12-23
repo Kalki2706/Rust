4b2abe79a3a2d7ed2c6a5285c7281cba6310bbf1
Kalki <kalkithewarrior2706@proton.me>
2023-12-23 20:20:06 +0530

    Comments in code

---
+++ 01_concepts/05_comments/src/main.rs
@@ -0,0 +1,12 @@
+// Single Line Comment.
+
+// So we're doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully this comment will
+// explain what's going on.
+
+// comments in the code
+
+fn main() {
+    // I’m feeling lucky today
+    let lucky_number = 7; // I’m feeling lucky today
+}
