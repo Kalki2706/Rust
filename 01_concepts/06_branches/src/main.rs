2d1cd04ad7e55d1358b24f83f16e6090d105bd41
Kalki <kalkithewarrior2706@proton.me>
2023-12-28 22:11:50 +0530

    add if else code

---
+++ 01_concepts/06_branches/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut number = 7;
+    
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // condition must be a **bool**
+   /* 
+    let number = 3;
+
+    if number {
+        println!("number was three");
+    } 
+    */
+
+    let number = 3;
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+}
