dbe5e0dc1bf028b526de7066241254fe23fa7b84
Kalki <kalkithewarrior2706@proton.me>
2024-01-24 20:57:09 +0530

    Add scope in variables

--- 04_practice/variables/src/main.rs
+++ 04_practice/variables/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-
+    
 // BINDING & MUTABILITY
 // A variable can be used only if it has been initialized.
 
@@ -25,5 +25,35 @@ fn main() {
         println!("Success!");
 
     // SOLUTION: Annotating i32 to x: not necessary, filled mut & x variable, assigned += to x variable
+    }
+
+// SCOPE
+// A scope is the range within the program for which the item is valid.
+
+    {
+    // 3. Fix the error below with least amount of modification
+
+        let x: i32 = 10;
+        let y: i32 = 5;
+
+        {
+            println!("The value of x is {} and value of y is {}", x, y);
+        }
+
+        println!("The value of x is {} and value of y is {}", x, y);
+
+    // SOLUTION: You can't access variable inside of a scope, so I removed this line `let y: i32 = 5;` from inner scope and put it outside of that block to not get error.
+    }
+
+    {
+    // 4. Fix the error with the use of define_x
+
+        fn define_x() {
+            let x: &str = "hello";
+            println!("{}, world", x);
+        }
+        define_x();
+
+    // SOLUTION: called define_x() outside of it's block
     }
 }
